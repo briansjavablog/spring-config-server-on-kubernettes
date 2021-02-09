3afa2d47cd0f1bf84dc0c443964f51d516b0da6e
unknown <hannawaybrian@googlemail.com>
2021-02-09 23:48:44 +0000

    updates to config service

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -4,17 +4,16 @@ cd config-consumer-service/
 mvn clean install
 docker image rm -f briansjavablog/config-consumer-service:k8
 docker image build -t briansjavablog/config-consumer-service:k8 .
-#docker push briansjavablog/config-consumer-service:k8
+docker push briansjavablog/config-consumer-service:k8
 cd ..
 cd config-service/
 mvn clean install
 docker image rm -f briansjavablog/config-service:k8
 docker image build -t briansjavablog/config-service:k8 .
-#docker push briansjavablog/config-service:k8
+docker push briansjavablog/config-service:k8
 cd ..
 #docker-compose up
 cd Kubernetes
 kubectl delete -f config-server-cluster.yml
-kubectl get all
 kubectl apply -f config-server-cluster.yml
 kubectl get all
