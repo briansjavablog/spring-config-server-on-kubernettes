9e9ca28a907bbe79a32fa06b1bf0ded17a2bde70
unknown <hannawaybrian@googlemail.com>
2021-02-10 22:47:28 +0000

    some config and Dockerfile cleanup

---
+++ buildAndPushImages.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd config-consumer-service/
+mvn clean install
+docker image rm -f briansjavablog/config-consumer-service:k8
+docker image build -t briansjavablog/config-consumer-service:k8 .
+docker push briansjavablog/config-consumer-service:k8
+cd ..
+cd config-service/
+mvn clean install
+docker image rm -f briansjavablog/config-service:k8
+docker image build -t briansjavablog/config-service:k8 .
+docker push briansjavablog/config-service:k8
